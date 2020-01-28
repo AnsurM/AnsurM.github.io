384283460171b5f344ab0d1e66bf4c47122cfc47
AnsurM <ansurmehdi@gmail.com>
2020-01-28 15:19:14 +0500

    Updates

---
+++ precache-manifest.9f894a5b0618e3ced1fcc4d2e6ec4868.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e223e5ba6fa31bcdc1333a0289da60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8f44a9bf71f7c85a2f5",
+    "url": "/static/css/main.d86729c6.chunk.css"
+  },
+  {
+    "revision": "5a5dd13fb5e1f5eeb377",
+    "url": "/static/js/2.f175dcd4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f175dcd4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8f44a9bf71f7c85a2f5",
+    "url": "/static/js/main.9130e64b.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
